34d3115a848b81674466be1c9f759ef8a7e9bbb6
NeulChan-MBP12/15 <neulchance@gmail.com>
2023-12-23 13:33:29 +0900

    fornow commit

--- build/lib/compilation.ts
+++ build/lib/compilation.ts
@@ -1,8 +1,12 @@
 import os from 'os';
 import fs from 'fs'
-import path from 'path'
+import path, {basename} from 'path'
+import { pipeline } from 'node:stream/promises'
+import { Transform, Writable, Readable, Duplex } from 'node:stream'
 import ts from 'typescript'
 import * as util from './util'
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
+import Vinyl from 'vinyl'
 // import * as tsb from './tsb'
 
 function getTypeScriptCompilerOptions(src: string): ts.CompilerOptions {
@@ -24,35 +28,114 @@ function createCompile(src: string, build: boolean, emitError: boolean, transpil
   const tsb = require('./tsb') as typeof import('./tsb')
   
   const rootDir = path.join(__dirname, '../../');
+  console.log('😂😂😂😂', rootDir)
   const projectPath = path.join(rootDir, src, 'tsconfig.json')
   const overrideOptions = {...getTypeScriptCompilerOptions(src), inlineSources: Boolean(build)}
   if (!build) {
 		overrideOptions.inlineSourceMap = true;
 	}
 
-  // console.log(overrideOptions)
-  const compilation = tsb.creaete(
+  /* Creating a compiler among TSC, SWC, and So on. */
+  const compilation = tsb.create(
     projectPath,
     overrideOptions,
     {
-      verbose: false,
+      verbose: true,
       transpileOnly: Boolean(transpileOnly),
       transpileWithSwc: typeof transpileOnly !== 'boolean' && transpileOnly.swc
     }
   )
-  console.log(compilation)
-  /* const compilation = tsb.create(projectPath, overrideOptions, {
-		verbose: false,
-		transpileOnly: Boolean(transpileOnly),
-		transpileWithSwc: typeof transpileOnly !== 'boolean' && transpileOnly.swc
-	}); */
+
+  function pipeline() {
+
+  }
+  // const a = compilation().pipe(fs.createWriteStream('./aa.txt'))
+  
+
+  const myDuplex = new Duplex({
+    read(size) {
+      // ...
+    },
+    write(chunk, encoding, callback) {
+      // ...
+    },
+  });
+  // pipeline()
+  // console.log(compilation())
+  return compilation
+
 }
 
-export function transpileTask(src: string, out: string, swc: boolean) {
-  createCompile(src, false, true, {swc})
-  if (os.totalmem() < 4_000_000_000) {
-    throw new Error('compilation requires 4GB of RAM');
+export async function transpileTask(src: string, out: string, swc: boolean) {
+  /**
+   * ⓵
+   * Sourcing through glob
+   */
+  const sourcingFileURL = new Readable({
+    objectMode: true,
+    async read() {
+      const g3 = new Glob('src/**/*.ts', { withFileTypes: true })
+      // console.log(g3)
+      const tsFiles = await glob('src/**/*.ts', { ignore: 'node_modules/**' })
+      for (const tsFile of tsFiles) {
+        this.push(tsFile)
+      }
+      this.pause()
+    }
+  })
+  
+  /**
+   * ⓶
+   * Read file & Conver to Vinyl
+   */
+  const rootDir = path.join(__dirname, '../../');
+  const sourcingFile = new Transform({
+    readableObjectMode: true,
+    writableObjectMode: true,
+    // objectMode: true,
+    transform(data, encoding, callback) {
+      const file = fs.readFileSync(data.toString())
+      const vinyl = new Vinyl({
+        path: rootDir+data.toString(),
+        contents: file
+      })
+      this.push(vinyl)
+      callback()
+    }
+  })
+
+  /**
+   * ⓷
+   * Save it to out
+   */
+  const what = new Transform({
+    readableObjectMode: true,
+    writableObjectMode: true,
+    transform(data, encoding, callback) {
+      console.log(data)
+      callback()
+    }
+  })
+  
+  const traspile = createCompile(src, false, true, {swc})
+  try {
+    await pipeline(
+      sourcingFileURL,
+      sourcingFile,
+      traspile(),
+      // what,
+      // traspile(),
+      // removeSpaces,
+      // fs.createWriteStream('temp.js'),
+      // toUpper,
+      // (err) => {
+      //   console.log(err)
+      // }
+    )
+  } catch (error) {
+    console.error(error)
   }
+  // task()
 }
 
 
