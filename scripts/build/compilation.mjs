4af7c91456e2ff2e8d46ce1131fc0268b388d0ee
NeulChan-MBP12/15 <neulchance@gmail.com>
2023-12-24 01:15:36 +0900

    fornow del dir scripts

--- scripts/build/compilation.mjs
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export function createCompiler() {
-  const projectPath = path.join(__dirname, '../../', 'src', 'tsconfig.json');
-  console.log(projectPath)
-}
