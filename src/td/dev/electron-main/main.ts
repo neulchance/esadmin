1fc83b835dd80cfe07f9c000951790a97af014b0
NeulChan-MBP12/15 <neulchance@gmail.com>
2023-12-24 16:19:21 +0900

    update src

--- src/td/dev/electron-main/main.ts
+++ src/td/dev/electron-main/main.ts
@@ -1,3 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) TinyDeskDev. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { app, dialog } from 'electron';
+// import { unlinkSync } from 'fs';
+
 /**
  * The main VS Code entry point.
  *
@@ -7,4 +15,9 @@
  * are running at the same time.
  */
 class DevMain {
+  constructor() {
+    console.log('??')
+  }
 }
+
+// export default DevMain
