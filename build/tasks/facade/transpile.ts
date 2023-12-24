1c3136236c60ddae4d669e965e83e3ecc2b20161
NeulChan-MBP12/15 <neulchance@gmail.com>
2023-12-24 16:08:33 +0900

    del files

--- build/tasks/facade/transpile.ts
+++
@@ -1,7 +0,0 @@
-import * as compilation from '../../lib/compilation'
-
-async function main() {
-  compilation.transpileTask('src', 'out', true)
-}
-
-main()
--- build/tasks/facade/watch.ts
+++
@@ -1,7 +0,0 @@
-import * as compilation from '../../lib/compilation'
-
-async function main() {
-  compilation.watchTask('out', false)
-}
-
-main()
