7b1e250150f8027ac95e0f5daae39fde687ef73c
NeulChan <neulchance@gmail.com>
2024-01-01 02:24:09 +0900

    before concat bootstrap js

--- src/td/dev/electron-sandbox/workbench/workbench.js
+++ src/td/dev/electron-sandbox/workbench/workbench.js
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------------------------------------------------
+ *  Copyright (c) TinyDeskDev. All rights reserved.
+ *  Licensed under the UNLICENSED License. See License.txt in the project root for license information.
+ *---------------------------------------------------------------------------------------------------*/
+
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../../../../typings/require.d.ts" />
+
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
@@ -6,4 +14,4 @@ const func = async () => {
   console.log(response) // prints out 'pong'
 }
 
-func()
+func();
--- src/td/platform/instantiation/common/descriptors.ts
+++ src/td/platform/instantiation/common/descriptors.ts
@@ -1,7 +1,7 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------
+ *  Copyright (c) TinyDeskDev. All rights reserved.
+ *  Licensed under the UNLICENSED License. See License.txt in the project root for license information.
+ *---------------------------------------------------------------------------------------------------*/
 
 export class SyncDescriptor<T> {
 
