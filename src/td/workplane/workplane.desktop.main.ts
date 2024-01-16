f81fdbe2162f64c7bb6e939cf736f9dd980f7e4c
NeulChan <neulchance@gmail.com>
2024-01-17 00:35:48 +0900

    del workplane

--- src/td/workplane/workplane.desktop.main.ts
+++
@@ -1,8 +0,0 @@
-/*----------------------------------------------------------------------------------------------------
- *  Copyright (c) TinyDeskDev. All rights reserved.
- *  Licensed under the UNLICENSED License. See License.txt in the project root for license information.
- *---------------------------------------------------------------------------------------------------*/
-
-import 'td/workplane/electron-sandbox/desktop.main';
-
-export {main} from 'td/workplane/electron-sandbox/desktop.main';
