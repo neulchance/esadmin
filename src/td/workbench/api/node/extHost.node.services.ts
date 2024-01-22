6e69bb571d228ba224fc7471f25546958512670b
NeulChan <neulchance@gmail.com>
2024-01-22 20:56:49 +0900

    copy workbench/api/node

---
+++ src/td/workbench/api/node/extHost.node.services.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { InstantiationType, registerSingleton } from 'td/platform/instantiation/common/extensions';
+import { ExtHostTerminalService } from 'td/workbench/api/node/extHostTerminalService';
+import { ExtHostTask } from 'td/workbench/api/node/extHostTask';
+import { ExtHostDebugService } from 'td/workbench/api/node/extHostDebugService';
+import { NativeExtHostSearch } from 'td/workbench/api/node/extHostSearch';
+import { ExtHostExtensionService } from 'td/workbench/api/node/extHostExtensionService';
+import { NodeExtHostTunnelService } from 'td/workbench/api/node/extHostTunnelService';
+import { IExtHostDebugService } from 'td/workbench/api/common/extHostDebugService';
+import { IExtHostExtensionService } from 'td/workbench/api/common/extHostExtensionService';
+import { IExtHostSearch } from 'td/workbench/api/common/extHostSearch';
+import { IExtHostTask } from 'td/workbench/api/common/extHostTask';
+import { IExtHostTerminalService } from 'td/workbench/api/common/extHostTerminalService';
+import { IExtHostTunnelService } from 'td/workbench/api/common/extHostTunnelService';
+import { IExtensionStoragePaths } from 'td/workbench/api/common/extHostStoragePaths';
+import { ExtensionStoragePaths } from 'td/workbench/api/node/extHostStoragePaths';
+import { ExtHostLoggerService } from 'td/workbench/api/node/extHostLoggerService';
+import { ILogService, ILoggerService } from 'td/platform/log/common/log';
+import { NodeExtHostVariableResolverProviderService } from 'td/workbench/api/node/extHostVariableResolverService';
+import { IExtHostVariableResolverProvider } from 'td/workbench/api/common/extHostVariableResolverService';
+import { ExtHostLogService } from 'td/workbench/api/common/extHostLogService';
+import { SyncDescriptor } from 'td/platform/instantiation/common/descriptors';
+import { ISignService } from 'td/platform/sign/common/sign';
+import { SignService } from 'td/platform/sign/node/signService';
+
+// #########################################################################
+// ###                                                                   ###
+// ### !!! PLEASE ADD COMMON IMPORTS INTO extHost.common.services.ts !!! ###
+// ###                                                                   ###
+// #########################################################################
+
+registerSingleton(IExtHostExtensionService, ExtHostExtensionService, InstantiationType.Eager);
+registerSingleton(ILoggerService, ExtHostLoggerService, InstantiationType.Delayed);
+registerSingleton(ILogService, new SyncDescriptor(ExtHostLogService, [false], true));
+registerSingleton(ISignService, SignService, InstantiationType.Delayed);
+registerSingleton(IExtensionStoragePaths, ExtensionStoragePaths, InstantiationType.Eager);
+
+registerSingleton(IExtHostDebugService, ExtHostDebugService, InstantiationType.Eager);
+registerSingleton(IExtHostSearch, NativeExtHostSearch, InstantiationType.Eager);
+registerSingleton(IExtHostTask, ExtHostTask, InstantiationType.Eager);
+registerSingleton(IExtHostTerminalService, ExtHostTerminalService, InstantiationType.Eager);
+registerSingleton(IExtHostTunnelService, NodeExtHostTunnelService, InstantiationType.Eager);
+registerSingleton(IExtHostVariableResolverProvider, NodeExtHostVariableResolverProviderService, InstantiationType.Eager);
--- src/td/workbench/api/node/uriTransformer.ts
+++ src/td/workbench/api/node/uriTransformer.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import {UriParts, IRawURITransformer, URITransformer, IURITransformer} from 'td/base/common/uriIpc';
+import { UriParts, IRawURITransformer, URITransformer, IURITransformer } from 'td/base/common/uriIpc';
 
 /**
  * ```
@@ -19,19 +19,19 @@ function createRawURITransformer(remoteAuthority: string): IRawURITransformer {
 	return {
 		transformIncoming: (uri: UriParts): UriParts => {
 			if (uri.scheme === 'vscode-remote') {
-				return {scheme: 'file', path: uri.path, query: uri.query, fragment: uri.fragment};
+				return { scheme: 'file', path: uri.path, query: uri.query, fragment: uri.fragment };
 			}
 			if (uri.scheme === 'file') {
-				return {scheme: 'vscode-local', path: uri.path, query: uri.query, fragment: uri.fragment};
+				return { scheme: 'vscode-local', path: uri.path, query: uri.query, fragment: uri.fragment };
 			}
 			return uri;
 		},
 		transformOutgoing: (uri: UriParts): UriParts => {
 			if (uri.scheme === 'file') {
-				return {scheme: 'vscode-remote', authority: remoteAuthority, path: uri.path, query: uri.query, fragment: uri.fragment};
+				return { scheme: 'vscode-remote', authority: remoteAuthority, path: uri.path, query: uri.query, fragment: uri.fragment };
 			}
 			if (uri.scheme === 'vscode-local') {
-				return {scheme: 'file', path: uri.path, query: uri.query, fragment: uri.fragment};
+				return { scheme: 'file', path: uri.path, query: uri.query, fragment: uri.fragment };
 			}
 			return uri;
 		},
