546d9f48c47f00621ed19fe0dc2491517cb635ec
NeulChan-MBP12/15 <neulchance@gmail.com>
2024-02-06 01:58:07 +0900

    Fix import formatting in extHostLoggerService.ts

--- src/td/workbench/api/node/extHostLoggerService.ts
+++ src/td/workbench/api/node/extHostLoggerService.ts
@@ -3,12 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ILogger, ILoggerOptions, ILoggerResource, LogLevel } from 'td/platform/log/common/log';
-import { URI } from 'td/base/common/uri';
-import { ExtHostLoggerService as BaseExtHostLoggerService } from 'td/workbench/api/common/extHostLoggerService';
-import { Schemas } from 'td/base/common/network';
-import { SpdLogLogger } from 'td/platform/log/node/spdlogLog';
-import { generateUuid } from 'td/base/common/uuid';
+import {ILogger, ILoggerOptions, ILoggerResource, LogLevel} from 'td/platform/log/common/log';
+import {URI} from 'td/base/common/uri';
+import {ExtHostLoggerService as BaseExtHostLoggerService} from 'td/workbench/api/common/extHostLoggerService';
+import {Schemas} from 'td/base/common/network';
+import {SpdLogLogger} from 'td/platform/log/node/spdlogLog';
+import {generateUuid} from 'td/base/common/uuid';
 
 export class ExtHostLoggerService extends BaseExtHostLoggerService {
 
