bb10ec0d5a992dab2adf3acd17aa548b33f6a201
NeulChan-MBP12/15 <neulchance@gmail.com>
2024-02-07 17:02:57 +0900

    relate terminal

---
+++ src/td/workbench/contrib/search/common/notebookSearch.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import {createDecorator} from 'td/platform/instantiation/common/instantiation';
+import {CancellationToken} from 'td/base/common/cancellation';
+import {ResourceSet} from 'td/base/common/map';
+import {ITextQuery, ISearchProgressItem, ISearchComplete} from 'td/workbench/services/search/common/search';
+
+export const INotebookSearchService = createDecorator<INotebookSearchService>('notebookSearchService');
+
+export interface INotebookSearchService {
+
+	readonly _serviceBrand: undefined;
+
+	notebookSearch(query: ITextQuery, token: CancellationToken | undefined, searchInstanceID: string, onProgress?: (result: ISearchProgressItem) => void): {
+		openFilesToScan: ResourceSet;
+		completeData: Promise<ISearchComplete>;
+		allScannedFiles: Promise<ResourceSet>;
+	};
+}
